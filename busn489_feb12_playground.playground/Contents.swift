f411fdf3c2444d360f8c7a78ec36012dfbcaad8e
andrewsorescu-tamu <andrewsorescu@tamu.edu>
2024-03-14 00:27:15 -0500

    continuing to learn swift

---
+++ busn489_feb12_playground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// if-else statements
+
+let a=false
+let b=true
+let c=true
+let d=false
+
+if a {
+    print("a is true")
+}
+else if !a || !b {
+    print("a and b are false")
+}
+else {
+    print("a is false")
+}
